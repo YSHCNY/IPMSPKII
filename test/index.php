4e128b38493f732c19b6e7fc4df11b814e47fbcc
Yoshiyuki Canoy <ycanoy@PHILKOEI.COM.PH>
2024-02-19 14:53:45 +0800

    sage na

--- test/index.php
+++ test/index.php
@@ -30,7 +30,7 @@
 <nav class="navbar navbar-expand-lg navbar-light sticky-top " id="navbar">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="./img/logo.png" class="img-fluid my-5 md-float-start" alt="">
+            <img src="./img/logo.png" class="img-fluid my-3 md-float-start" alt="">
         </a>
         <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -41,16 +41,28 @@
                     <button type="button" class="nav-link rounded  border-0 text-light my-md-5 my-1 p-2 mx-2 fw-medium" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg text-light fw-medium" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-                        </svg> IP ADDRESS
+                        </svg> IP Address
                     </button>
                 </li>
                 <li class="nav-item">
-                    <button type="button" class="nav-link rounded border-0  my-md-5 my-1  mx-2 p-2  text-light fw-medium" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <a type="button" class="nav-link rounded border-0  my-md-5 my-1  mx-2 p-2  text-light fw-medium" data-bs-toggle = "modal" data-bs-target="#exampleModal">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg text-light" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-                        </svg> IP TYPE
-                    </button>
+                        </svg> IP Type </a>
                 </li>
+
+                <li class="nav-item">
+                    <a type="button" class="nav-link rounded border-0  my-md-5 my-1  mx-2 p-2  text-light fw-medium" href = "https://www.speedtest.net/run"> 
+                       Speed Test <span class = "fs-6 text-light">(ookla)</span>
+                        </a>
+                </li>
+
+             
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-speedometer2 text-light" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4M3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.39.39 0 0 0-.029-.518z"/>
+                <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A8 8 0 0 1 0 10m8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3"/>
+                </svg> -->
+
 
                 <li class="nav-item ">
                 <button class="text-light border-0 bg-transparent my-md-5 my-1 float-end ms-2 p-2 " onclick="refreshSelect()">
